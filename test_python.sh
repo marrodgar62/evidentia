0974608113605f84f9c4eb789c505f9e278d43f6
Mario <marpercor8@alum.us.es>
2022-12-18 09:50:55 +0100

    CHROME DEPENDENCE IN TEST VIEW -FIX: Chrome dependence.

--- test_python.sh
+++ test_python.sh
@@ -1,5 +1,7 @@
+sudo apt -y install google-chrome-stable 
 python -m venv /tmp/venvidentia/
 source /tmp/venvidentia/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+
 python -m unittest discover tests/TestSuiteVista/ "*_test.py"
